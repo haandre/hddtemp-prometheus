0dd8dd2d9eef10eeee16a52d10a0f27552a62b93
haandre <andre.haehnel@t-online.de>
2022-01-02 12:11:25 +0100

    Added nvme support

--- hddtemp-prometheus.sh
+++ hddtemp-prometheus.sh
@@ -7,16 +7,16 @@ fi
 
 if [[ -z "${FOLDER}" ]];then
     echo "environment variable FOLDER missing!"
-    exit 1
+   exit 1
 fi
 
 if [[ -z "${FILE}" ]];then
     echo "environment variable FILE missing!"
-    exit 1
+   exit 1
 fi
 
 if [ ! -d $FOLDER ]; then
-    mkdir -p $FOLDER
+   mkdir -p $FOLDER
 fi
 
 LIST=$(ls /dev/sd[a-z])
@@ -28,5 +28,14 @@ do
     TMP="node_hwmon_temp_celsius{chip=\"$DISK\",sensor=\"temp1\",serial=\"$SERIAL\"} $TEMP\n"
     OUTPUT="$OUTPUT$TMP"
     
+done
+
+LIST=$(ls /dev/nvme[0-9]n1)
+for DISK in $LIST
+do
+    SERIAL=$(udevadm info --query=property --name=$DISK | grep ID_SERIAL_SHORT | cut -d "=" -f 2)
+    TEMP=$(smartctl --all /dev/nvme1n1 | grep Temperature: | grep -Po '\d{1,3}')
+    TMP="node_hwmon_temp_celsius{chip=\"$DISK\",sensor=\"temp1\",serial=\"$SERIAL\"} $TEMP\n"
+    OUTPUT="$OUTPUT$TMP"
 done
 echo -e $OUTPUT > $FOLDER/$FILE
